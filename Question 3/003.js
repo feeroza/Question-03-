21c69556b7a9bec869950c0671e67f3bec061048
feeroza <160626229+feeroza@users.noreply.github.com>
2024-08-08 21:53:21 +0500

    Add files via upload

---
+++ Question 3/003.js
@@ -0,0 +1,7 @@
+//3. Name Cases: Store a person’s name in a variable, and then print that person’s name in
+// lowercase, uppercase, and titlecase.
+//tittal cas
+var myName = "Feeroza";
+console.log(myName);
+console.log(myName.toLowerCase());
+console.log(myName.toUpperCase());
---
+++ Question 3/003.ts
@@ -0,0 +1,15 @@
+//3. Name Cases: Store a person’s name in a variable, and then print that person’s name in
+// lowercase, uppercase, and titlecase.
+
+
+
+//tittal cas
+let myName: string = "Feeroza";
+
+console.log(myName)
+
+console.log(myName.toLowerCase());
+
+console.log(myName.toUpperCase());
+
+
